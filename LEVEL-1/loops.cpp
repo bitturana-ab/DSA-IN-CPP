d3dae9bb0d2562e303565e3cb51d88e1b6e9ab87
AB <AB@AB.com>
2025-04-30 22:28:45 +0530

    Add patterns for square, repeated numbers, and sequential printing in loops

--- LEVEL-1/loops.cpp
+++ LEVEL-1/loops.cpp
@@ -49,4 +49,55 @@ int main()
     {
         cout << "Number is prime " << endl;
     }
+
+    // pattern for square * printing
+    int row;
+    cout << "Enter number of rows: ";
+    cin >> row;
+    int i1 = 1;
+    while (i1 <= row)
+    {
+        int j = 1;
+        while (j <= row)
+        {
+            cout << "* ";
+            j++;
+        }
+        cout << endl;
+        i1++;
+    }
+
+    // pattern for 1 1 1 1 ,2 2 2 2 ... n n n n number printing
+    int n1;
+    cout << "Enter number of levels: ";
+    cin >> n1;
+    int i2 = 1;
+    while (i2 <= n1)
+    {
+        int j = 1;
+        while (j <= n1)
+        {
+            cout << i2 << " ";
+            j++;
+        }
+        cout << endl;
+        i2++;
+    }
+
+    // pattern for 1 2 3 4 .. n printing in each row
+    int row1;
+    cout << "Enter number of rows: ";
+    cin >> row1;
+    int count1 = 1;
+    while (count1 <= row1)
+    {
+        int j = 1;
+        while (j <= row1)
+        {
+            cout << j << " ";
+            j++;
+        }
+        cout << endl;
+        count1++;
+    }
 }
