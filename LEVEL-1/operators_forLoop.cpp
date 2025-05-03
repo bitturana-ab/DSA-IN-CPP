314e6e296198a3d2ce1fb9f10507b1ec1150db5b
AB <AB@AB.com>
2025-05-03 15:51:29 +0530

    problems solving on LeetCode

--- LEVEL-1/operators_forLoop.cpp
+++ LEVEL-1/operators_forLoop.cpp
@@ -2,116 +2,120 @@
 using namespace std;
 int main()
 {
+
+    // uc-comments which part you want to execute
+    /*
+
     // BITWISE OPERATORS
     // 1. AND -> &
     // 2. OR -> |
     // 3. NOT -> ~
     // 4. XOR -> ^
 
-    // int a = 4; // 000..0100
-    // int b = 6; // 000..0111
+    int a = 4; // 000..0100
+    int b = 6; // 000..0111
 
-    // cout << " a & b = " << (a & b) << endl;
-    // cout << " a | b = " << (a | b) << endl;
-    // cout << " ~ a = " << ~a << endl;
-    // cout << " a ^ b = " << (a ^ b) << endl;
+    cout << " a & b = " << (a & b) << endl;
+    cout << " a | b = " << (a | b) << endl;
+    cout << " ~ a = " << ~a << endl;
+    cout << " a ^ b = " << (a ^ b) << endl;
 
     // left shift || right shift operators
     // left shift
     // 5 << 1 ==> 5 = 0101 => 1010 = 10
-    // cout << " 5 << 1 = " << (5 << 1) << endl;
+    cout << " 5 << 1 = " << (5 << 1) << endl;
     // // 5 << 2 ==> 5 = 0101 => 0100 = 20
-    // cout << " 5 << 2 = " << (5 << 2) << endl;
+    cout << " 5 << 2 = " << (5 << 2) << endl;
 
     // // right shift
     // // 5 >> 1 ==> 5 = 0101 => 010 = 2
-    // cout << " 5 >> 1 = " << (5 >> 1) << endl;
+    cout << " 5 >> 1 = " << (5 >> 1) << endl;
     // // 5 >> 2 ==> 5 = 0101 => 01 = 1
-    // cout << " 5 >> 2 = " << (5 >> 2) << endl;
+    cout << " 5 >> 2 = " << (5 >> 2) << endl;
     // // 0101 => 0
-    // cout << " 5 >> 3 = " << (5 >> 3) << endl;
+    cout << " 5 >> 3 = " << (5 >> 3) << endl;
     // // 0101 =>
-    // cout << " 5 >> 4 = " << (5 >> 4) << endl;
+    cout << " 5 >> 4 = " << (5 >> 4) << endl;
 
     // trick
     // right >> num / 2
-    // cout << (5 >> 1) << endl;
-    // cout << (18 >> 2) << endl;
+    cout << (5 >> 1) << endl;
+    cout << (18 >> 2) << endl;
 
     // // left shift << num * 2
-    // cout << (3 << 1) << endl;
-    // cout << (4 << 2) << endl;
+    cout << (3 << 1) << endl;
+    cout << (4 << 2) << endl;
 
-    // int i = 7;
-    // cout << (++i) << endl; // pre-increment
+    int i = 7;
+    cout << (++i) << endl; // pre-increment
     // // print = 8 , i = 8
-    // cout << (i++) << endl; // post-increment
+    cout << (i++) << endl; // post-increment
     // // print = 8 , i = 9
-    // cout << (i--) << endl; // pre-decrement
+    cout << (i--) << endl; // pre-decrement
     // // print = 9 , i = 8
-    // cout << (--i) << endl; // post-decrement
+    cout << (--i) << endl; // post-decrement
     // // print = 7 , i = 7
 
-    // int number = 3;
-    // cout << (25 * (++number)) << endl;
+    int number = 3;
+    cout << (25 * (++number)) << endl;
 
     // for loops
-    // int n;
-    // cout << "Enter a number: ";
-    // cin >> n;
-    // for (int i = 1; i <= n; i++)
-    // {
-    //     cout << i << endl;
-    // }
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        cout << i << endl;
+    }
     // other way
-    // int i = 1;
-    // for (;;)
-    // {
-    //     if (i <= n)
-    //     {
-    //         cout << i << endl;
-    //     }
-    //     else
-    //     {
-    //         break;
-    //     }
-    //     i++;
-    // }
+    int i = 1;
+    for (;;)
+    {
+        if (i <= n)
+        {
+            cout << i << endl;
+        }
+        else
+        {
+            break;
+        }
+        i++;
+    }
 
     // multiple initialization and multiple conditions in for loop
-    // for (int a = 0, b = 1; a >= 0 && b >= 0; a--, b--)
-    // {
-    //     cout << a << " " << b << " " << endl;
-    // }
+    for (int a = 0, b = 1; a >= 0 && b >= 0; a--, b--)
+    {
+        cout << a << " " << b << " " << endl;
+    }
 
     // sum of n number
-    // int n;
-    // cout << "Enter a number: ";
-    // cin >> n;
-    // int sum = 0;
-    // for (int i = 1; i <= n; i++)
-    // {
-    //     // sum = sum + i;
-    //     sum += i;
-    // }
-    // cout << "sum of " << n << " is: " << sum << endl;
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    int sum = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        // sum = sum + i;
+        sum += i;
+    }
+    cout << "sum of " << n << " is: " << sum << endl;
 
     // fibonacci series
     // 0 1 1 2 3 5 8 13
-    // int n;
-    // cout << "Enter a number for steps: ";
-    // cin >> n;
-    // int back = 0;
-    // int next = 1;
-    // cout << back << " ";
-    // cout << next << " "; // 0 1
-    // for (int i = 1; i <= n; i++)
-    // {
-    //     int sum = (back + next); // sum = 0+1
-    //     cout << sum << " ";
-    //     back = next;
-    //     next = sum;
-    // }
+    int n;
+    cout << "Enter a number for steps: ";
+    cin >> n;
+    int back = 0;
+    int next = 1;
+    cout << back << " ";
+    cout << next << " "; // 0 1
+    for (int i = 1; i <= n; i++)
+    {
+        int sum = (back + next); // sum = 0+1
+        cout << sum << " ";
+        back = next;
+        next = sum;
+    }
 
     // prime number
     int n;
@@ -132,4 +136,16 @@ int main()
     {
         cout << "Number is prime. ";
     }
+
+    // continue ==> skip and go to next iteration
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "Hii" << endl;
+        cout << "Suno na" << endl;
+        continue; // goesto next iteration and skip this next work
+        cout << "Reply kr de.." << endl;
+    }
+    */
+
+    // ahead problem solved in leetcode (bitturana_ab)
 }
