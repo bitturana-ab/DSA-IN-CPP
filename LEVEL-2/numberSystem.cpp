b306a7fdb492b82ae66a6cd90af03fc310c80df0
AB <AB@AB.com>
2025-05-04 13:03:48 +0530

    binary to decimal

--- LEVEL-2/numberSystem.cpp
+++ LEVEL-2/numberSystem.cpp
@@ -19,34 +19,67 @@ int main()
     // int i = 0;
     // while (n != 0)
     // {
-    //     int bit = n & 1;                // 0101 & 1 (each time last one bit aquire after right shifting)
-    //     ans = (bit * pow(10, i) + ans); // store in integer in reverse order of 1 -> 0 -> 1 -> 0
-    //     n = n >> 1;                     // one bit right shift
+    //     int last_bit = n & 1; // 0101 & 1 (each time last one bit aquire after right shifting)
+    //     ans = ans * 10 + last_bit;
+    //     // ans = (bit * pow(10, i) + ans);
+    //     n = n >> 1; // one bit right shift of binary of n
     //     i++;
     // }
     // cout << "Answer is: " << ans << endl;
 
     // print as it is number
-    int n = 132;
-    int ans = 0;
-    int i = 0;
-    while (n != 0)
-    {
-        int digit = n % 10;
-        ans = (digit * pow(10, i) + ans);
-        n = n / 10;
-        i++;
-    }
+    // int n = 132;
+    // int ans = 0;
+    // int i = 0;
+    // while (n != 0)
+    // {
+    //     int digit = n % 10;
+    //     // ans = ans * 10 + digit; // reverse cause last digit
+    //     ans = (digit * pow(10, i) + ans);
+    //     n = n / 10;
+    //     i++;
+    // }
 
-    cout << "Number is " << ans << endl;
+    // cout << "Number is " << ans << endl;
 
     // reverse a number
-    int num = 893247;
-    int rev_num = 0;
-    while (num > 0)
+    // int n = 7835392;
+    // int n1 = n;      // for print
+    // int rev_num = 0; // initialize with 0
+    // while (n != 0)
+    // {
+    //     int rem_digit = n % 10; // get last digit of n
+    //     rev_num = (rev_num * 10) + rem_digit;
+    //     n = n / 10; // cut last digit of n
+    // }
+    // cout << "Reverse of " << n1 << " : " << rev_num << endl;
+
+    // binary to decimal
+    // int n;
+    // cout << "Enter binary digits: "; // binary as an integer i will optimize in next level
+    // cin >> n;
+    // int i = 0, ans = 0;
+    // while (n != 0)
+    // {
+    //     int last_bit = n % 10; // 0 || 1
+    //     if (last_bit == 1)
+    //     {
+    //         ans = ans + pow(2, i);
+    //     }
+    //     n = n / 10;
+    //     i++;
+    // }
+    // cout << ans << endl;
+
+    // reverse an integer for leetcode
+    int n;
+    cin >> n;
+    int ans = 0;
+    while (n != 0)
     {
-        rev_num = rev_num * 10 + num % 10;
-        num = num / 10;
+        int last_digit = n % 10;
+        ans = ans * 10 + last_digit;
+        n = n / 10;
     }
-    cout << rev_num << endl;
+    cout << ans << endl;
 }
